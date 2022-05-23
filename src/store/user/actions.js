b1630ce0554d5d0829166bc8839f1ad174a41845
Andrew  Sauer <andrewjsauer@users.noreply.github.com>
2022-05-23 10:48:16 -0600

    Update actions.js

--- src/store/user/actions.js
+++ src/store/user/actions.js
@@ -1,5 +1,5 @@
 import { createAsyncAction, createAsyncTypes } from 'lib/reduxUtils';
 
 // Async
-export const VALIDATE_USER = createAsyncTypes('auth/validateUser');
+export const VALIDATE_USER = createAsyncTypes('user/validateUser');
 export const validateUser = createAsyncAction(VALIDATE_USER);
